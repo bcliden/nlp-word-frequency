d63a65b6cea3b86afe56eb8d54b140d855f6bf79
Benjamin Liden <benjamin.c.liden@gmail.com>
2019-07-19 13:58:38 -0400

    added .envrc to auto-activate venv, added config auto-set

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+app.config.from_object(os.environ["APP_SETTINGS"])
+
+print(f"Loading config: {os.environ['APP_SETTINGS']}")
 
 
 @app.route("/")
